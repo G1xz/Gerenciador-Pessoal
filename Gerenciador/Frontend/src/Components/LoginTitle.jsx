74d7fe39be554aec76f027663102f4b259446464
G1xz <165445754+G1xz@users.noreply.github.com>
2025-04-01 00:36:09 -0300

    Login new screen

--- Gerenciador/Frontend/src/Components/LoginTitle.jsx
+++ Gerenciador/Frontend/src/Components/LoginTitle.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const LoginTitle = () => {
   return (
-    <div>
-  <h2 className="font-montserrat text-2xl">Login</h2>
-  
+    <div className="flex relative gap-2 items-center justify-center text-center">
+  <h2 className="font-montserrat text-5xl font-semibold">Login</h2>
+  <img src="src/assets//Img/logo_furina_preta1.png" alt="img" className="h-[50px] bg-slate-400 rounded-full shadow-cyanMediumShadow"/>
 </div>
 
   );
